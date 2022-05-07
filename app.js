4141fddb06a007f7dd652fe25c99598f65961734
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-05-07 11:47:51 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+function getPin(){
+  const pin = Math.round(Math.random() * 10000);
+  const pinString = pin + '';
+  if(pinString.length == 4){
+    return pin;
+  }
+  else{
+    console.log('get 3 digit pin and calling again');
+    return getPin();
+  }
+}
+
+function generatePin(){
+  const pin = getPin();
+  //console.log(pin);
+  document.getElementById('display-pin').value = pin;
+}
+
+ document.getElementById('key-pad').addEventListener('click', function(event){
+ const number =  event.target.innerText;
+ const calcInput =  document.getElementById('typed-numbers');
+ if(isNaN(number)){
+     console.log(number);
+     if(number == 'C'){
+       calcInput.value = '';
+   }
+ }
+  else{
+     const previusNumber = calcInput.value;
+     const newCalc = previusNumber + number;
+     calcInput.value = newCalc;
+  }
+ })
+
+ function varifyPin(){
+  // console.log('going to veryfy')
+  const pin = document.getElementById('display-pin').value;
+  const typedNumbers = document.getElementById('typed-numbers').value;
+  const successMessege = document.getElementById('notify-success');
+  const failError = document.getElementById('notify-fail');
+  if(pin == typedNumbers){
+    //const successMessege = document.getElementById('notify-success');
+    successMessege.style.display = 'block';
+    failError.style.display = 'none';
+  }
+  else{
+    //const failError = document.getElementById('notify-fail');
+    failError.style.display = 'block';
+    successMessege.style.display = 'none';
+  }
+ }
+//   // practice
+//  function getPin(){
+//    const pin = Math.round(Math.random() * 10000);
+//    const pinString = pin + '';
+//    if(pinString.length == 4){
+//      return pin;
+//    }
+//    else{
+//      return getPin();
+//    }
+//  }
+//  function generatePin(){
+//   const pin = getPin();
+//   document.getElementById('display-pin').value = pin;
+//  }
+
+//  document.getElementById('key-pad').addEventListener('click', function(event){
+//    const number = event.target.innerText;
+//    const calcInput = document.getElementById('typed-numbers');
+//    if(isNaN(number)){
+//     if(number == 'C'){
+//       calcInput.value = '';
+//     }
+//    }
+//    else{
+//    const previusNumber = calcInput.value;
+//    const newCalc = previusNumber + number;
+//    calcInput.value = newCalc;
+//    }
+//  })
